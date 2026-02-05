eecf1dba5c0c880f80765ead8741e1364a8c4bc2
Guilherme Tinem <guizstinem@gmail.com>
2026-02-05 12:19:55 -0300

    refactor: implemented Buy All Items functionality

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { addToCart,removeFromCart, incrementQuantity, decrementQuantity, checkOut, clearCart, buyAll} = require('../controllers/cartController')
+const verifyToken = require('../middlewares/verifyToken')
+const cartRouter = express.Router()
+
+
+cartRouter.post("/add",verifyToken,addToCart)
+cartRouter.delete("/remove/:id",verifyToken,removeFromCart)
+cartRouter.post("/increment/:id",verifyToken,incrementQuantity)
+cartRouter.post("/decrement/:id",verifyToken,decrementQuantity)
+cartRouter.post("/checkout",verifyToken,checkOut)
+cartRouter.get("/clear",verifyToken,clearCart)
+cartRouter.post("/buy-all",verifyToken,buyAll)
+
+
+module.exports = cartRouter
